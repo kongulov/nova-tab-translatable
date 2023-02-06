a54c963a848ec5ed857e610a6de714c0fbfb6ce2
Ramiz Kongulov <ramiz.kongulov@gmail.com>
2023-02-06 11:18:32 +0400

    Fixed issue #33 (Problem with slug preview)

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/nova-api/{resource}/{resourceId}/download/{field}', 'FieldDownloadController@show');
 Route::delete('/nova-api/{resource}/{resourceId}/field/{field}', 'FieldDestroyController@handle');
 Route::delete('/nova-api/{resource}/{resourceId}/{relatedResource}/{relatedResourceId}/field/{field}', 'PivotFieldDestroyController@handle');
+Route::post('/nova-api/{resource}/field/{field}/preview', 'FieldPreviewController@invoke');
---
+++ src/Http/Controllers/FieldPreviewController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Kongulov\NovaTabTranslatable\Http\Controllers;
+
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Kongulov\NovaTabTranslatable\NovaTabTranslatable;
+use Laravel\Nova\Contracts\Previewable;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+class FieldPreviewController extends Controller
+{
+    /**
+     * Delete the file at the given field.
+     *
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws AuthorizationException
+     */
+    public function invoke(NovaRequest $request)
+    {
+        $resource = $request->newResource();
+
+        $explode = explode('_', $request->field);
+        $locale = last($explode);
+        $fieldNameArray = array_slice($explode, 1, -1);
+        $fieldName = implode('_', $fieldNameArray);
+
+        if (($resource->translatable === null && $fieldName === '') || !in_array($fieldName, $resource->translatable)) { // not translatable file
+            $controller = new \Laravel\Nova\Http\Controllers\FieldPreviewController;
+
+            return $controller($request);
+        }
+
+        $tabs = $resource->updateFields($request)->whereInstanceOf(NovaTabTranslatable::class);
+
+        /** @var \Laravel\Nova\Fields\Field&\Laravel\Nova\Contracts\Previewable&false $field */
+        $field = false;
+
+        foreach ($tabs as $tab) {
+            $field = collect($tab->data)->first(function($field) use ($request){
+                return isset($field->attribute) &&
+                    $field->attribute == $request->field;
+            });
+        }
+
+        if (!$field) abort(404);
+
+        $request->validate(['value' => ['nullable', 'string']]);
+
+        return response()->json([
+            'preview' => $field->previewFor($request->value),
+        ]);
+    }
+}
